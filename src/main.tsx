02ead49dc7b0319d255139ac3f7e7b3f96020bc4
karim chedli frini <chedlifrini@gmail.com>
2025-03-29 19:22:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="414739204309-6tg5ljlcn2d0oofgb51sulcrere6u44h.apps.googleusercontent.com">
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
