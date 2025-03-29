473f6a4b948750710094354ccbafb975173316f4
karim chedli frini <chedlifrini@gmail.com>
2025-03-29 19:19:57 +0100

    Update auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,3 +6,7 @@ export async function login(UserLogin: UserLogin) {
     const response = await axios.post(`${apiUrl}/auth/login`, UserLogin);
     return response.data;
 }
+export async function googleLogin(token: string) {
+    const response = await axios.post(`${apiUrl}/auth/google-login`, { token });
+    return response.data;
+}
