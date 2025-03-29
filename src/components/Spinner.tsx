b6e9206b2d1aa86b534b1fcc11af3a6ab2e526d4
karim chedli frini <chedlifrini@gmail.com>
2025-03-30 00:56:36 +0100

    Create Spinner.tsx

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import './Spinner.css'; // Add your spinner styles here
+
+const Spinner = () => {
+    return (
+        <div className="spinner">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default Spinner;
